ea9753df064027f0a6bbfc967c497171dfb58d29
Sonny Mai <smai@dius.com.au>
2014-11-29 17:09:55 +1100

    Default route to go to items

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ BenjamRails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'items#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
