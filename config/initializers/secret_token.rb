e19248a4f060bbf1b56acbc18f0c0b96578eb988
Brad Garland <brad@bradgarland.com>
2014-11-29 13:40:34 +1100

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BenjamRails::Application.config.secret_key_base = '064d9434744ddd20ba4a51c9c40302d23466f64f13154c95cb9b006d9511c6fc10aae6b19cb26b4061b19a029c74ac7a21724bb9631d26c6de8ecc9d34767a7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BenjamRails::Application.config.session_store :cookie_store, key: '_benjam-rails_session'
