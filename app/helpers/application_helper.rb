e87e5ec0d25812164c9d9b93fb39a31af1577ee1
Sonny Mai <smai@dius.com.au>
2015-06-13 16:15:52 +1000

    get working for production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def paths_for_manifests(manifests = [])
     manifests.map do |manifest|
-      Rails.application.assets[manifest].dependencies.map{|d| "/assets/#{d.logical_path}"}
+      Rails.application.assets[manifest].dependencies.map{|d| "#{d.logical_path}"}
     end.flatten
   end
 end
