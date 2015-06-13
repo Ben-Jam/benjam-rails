7731e729a76751b953e14ee981deca8d107a1e58
Sonny Mai <smai@dius.com.au>
2015-06-13 15:57:02 +1000

    Manifest for css

--- app/controllers/cache_controller.rb
+++ app/controllers/cache_controller.rb
@@ -1,6 +1,8 @@
 class CacheController < ApplicationController
   def index
   	@items = Item.all
+    @environment = Rails.application.assets
+    @asset_list = @environment.each_logical_path(*Rails.application.config.assets).to_a
 
   	render layout: false
   	response.headers['Content-Type'] = 'text/cache-manifest'
