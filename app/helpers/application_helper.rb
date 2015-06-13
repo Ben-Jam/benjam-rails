dc06d3abecabd8d4ca57a27a29968d11c9f8e078
Sonny Mai <smai@dius.com.au>
2015-06-13 16:28:33 +1000

    Clean up offline manifest

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
       Rails.application.assets[manifest].dependencies.map{|d| "#{d.logical_path}"}
     end.flatten
   end
+
+  def build_digested_asset_path(asset)
+    environment = Rails.application.assets
+    "/assets/#{environment.find_asset(asset).digest_path}" rescue nil
+  end
 end
